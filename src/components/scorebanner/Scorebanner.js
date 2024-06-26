7845a3f95f5ac08494ccabc6ed70a04a1ab35c7b
ishtiaq_ahmad <ishtiaqahmad427@gmail.com>
2024-06-26 09:32:53 +0500

    working on score banner yet

--- src/components/scorebanner/Scorebanner.js
+++ src/components/scorebanner/Scorebanner.js
@@ -1,12 +1,38 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./Scorebanner.css";
+import cornerstyle from "../../images/scorebanner/cornerstylewebp.webp";
+import subjectimg from "../../images/scorebanner/subject.png";
 
 export default function Scorebanner() {
   return (
     <div className="cardcontainer">
-      <div className="insidecont contentcont ">
-        <div className="mainreviewcont">
-          <h1 className="mainreviewheading"> Customers Reviews</h1>
+      <div className="insidecont contentcont scorecardcontainer">
+        <div className="scorecardcont">
+          <div className="scorecardleft">
+            <div className="scorecont">score</div>
+            <div className="scorecontent">
+              <p className="scoreheading">
+                Top rated by Students & immigration firms with{" "}
+                <span>100% success rate.</span>
+              </p>
+              <p className="scoresub">
+                Get free Study Abroad Assessment today!
+              </p>
+              <div className="actionbtncont">
+                <div class="apllyBtn">
+                  <Link to="#">
+                    <span>Apply Now</span>
+                  </Link>{" "}
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="scorecardcenter"></div>
+          <div className="scorecardright">
+            <img className="cornerstyle" src={cornerstyle} alt="" />
+            <img src={subjectimg} alt="" />
+          </div>
         </div>
       </div>
     </div>
